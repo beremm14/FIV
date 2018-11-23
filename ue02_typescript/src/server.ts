f8f83f464b94c5a1ccd6498ec92a2122d53e6c4b
Emil Berger <beremm14@htlkaindorf.at>
2018-11-23 11:45:39 +0100

    FIV 23.11.18 - Login

--- ue02_typescript/src/server.ts
+++ ue02_typescript/src/server.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 
 // Externes Modul (via npm installieren)
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 
 export class Server {
@@ -18,11 +19,13 @@ const assetsPath = path.join(__dirname, '..', 'assets');
         // Alle Dateien im assets-Ordner verwenden
         this._server.use('/', express.static(assetsPath));
 
+        this._server.use(bodyParser.json());
+        this._server.use(bodyParser.urlencoded());
+
+        this._server.post('/login.html', (req, res, next) => this.handlePostLogin(req, res, next));
+
         this._server.get('/liste',
             (req, res, next) => this.handleGetListe(req, res, next)
-        );
-        this._server.get('/image.png',
-            (req, res, next) => this.sendImage(res)
         );
     }
 
@@ -43,8 +46,9 @@ const assetsPath = path.join(__dirname, '..', 'assets');
         res.sendFile(filePath);
     }
 
-    private sendImage (res: express.Response) {
-        res.sendFile(path.join(__dirname, '..', 'assets', 'image.png'));
+    private handlePostLogin (req: express.Request, res: express.Response, next: express.NextFunction) {
+        debugger;
+        next();
     }
 
 }
