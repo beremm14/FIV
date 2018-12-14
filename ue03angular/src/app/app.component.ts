da393b667470b86ac7ceae787f5915c4e62570c8
Emil Berger <beremm14@htlkaindorf.at>
2018-12-14 11:48:36 +0100

    FIV 14.12.18 - List, css class

--- ue03angular/src/app/app.component.ts
+++ ue03angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getRenderedText } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ue03angular';
+  public title = 'ue03angular';
+  public clock = '??:??:??';
+  public items = [
+    {myClass: 'red', text: '1'},
+    {myClass: 'green', text: '2'},
+    {myClass: 'red', text: '3'}
+  ];
+  public myClass = 'green';
+
+  public constructor () {
+    this.clock = new Date().toLocaleTimeString();
+
+    setInterval( () => this.handleTimer(), 1000);
+  }
+
+  private handleTimer () {
+    this.clock = new Date().toLocaleTimeString();
+    // this.items.push('' + (+this.items[this.items.length - 1] + 1));
+  }
 }
