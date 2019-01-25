08eb19794f0068c118367f92509b1add9a5ba881
Emil Berger <beremm14@htlkaindorf.at>
2019-01-25 11:48:00 +0100

    FIV 25.01.19 - ng-navbar

--- ue04/src/app/app.module.ts
+++ ue04/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreesComponent } from './trees.component';
 import { CarsComponent } from './cars.component';
 import { ClockComponent } from './clock.component';
+import { NavBarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TreesComponent, CarsComponent, ClockComponent
+    AppComponent, TreesComponent, CarsComponent, ClockComponent, NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
